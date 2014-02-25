02e52782c0317b841f757fda49cef5426f0cd5cf
Wilco van Duinkerken <wilco@sparkboxx.com>
2014-02-25 16:46:46 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='KafNafParser',
+      version='1.0',
+      description = 'Parser between KAF and NAF',
+      author = 'Ruben Izquierdo',
+      author_email = 'r.izquierdobevia@vu.nl'
+      )
