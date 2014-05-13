9ec7db5b7aa3873220a95fc2d3b42c0b53ce046f
Giannis Melidis <giannismelidis@olery.com>
2014-05-13 13:31:33 +0200

    updated version.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='KafNafParser',
-      version='1.3',
+      version='1.4',
       description = 'Parser between KAF and NAF',
       author = 'Ruben Izquierdo',
       url = 'https://github.com/cltl/KafNafParserPy',
